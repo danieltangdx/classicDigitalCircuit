f54d9473d2559d3dc233b0e9694e1e12727ebb66
Tang dongxue <danieltangdx@outlook.com>
2017-12-20 16:59:39 +0800

    module of one bit full add

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # classicDigitalCircuit
-add file name giipushu
+Compile on Stratix iV 230 FPGA Report :
+
+OneBitFullAdd.v :
+
+| Combinational ALUTs | 2    |
+| ------------------- | ---- |
+| Total pins          | 5    |
+
--- oneBitFullAdd.v
+++ oneBitFullAdd.v
@@ -4,13 +4,12 @@
 	Description	:	Example of a one-bit full add
 	Revision	:	2017/12/20
 	Company		:	***********
+	Code	: 	Verilog-2011
 ************************/
 module oneBitFullAdd(
 input	iA,iB,iC,
 output	oSum,oCout
 	);
-wire 	oSum;
-wire	oCout;
 assign	oSum = iA ^ iB ^ iC;
 assign	oCout = (iA ^ iB) | (iB & iC) | (iA & iC);
 endmodule
